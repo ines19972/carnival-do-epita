aac2b0a5853c02d51a2ffe4336b8e457e6346f21
Ines Bargach <bargachines@gmail.com>
2019-09-25 21:05:19 +0200

    New stratetgy

--- src/Game/PlayerIA/Ines19972Player.php
+++ src/Game/PlayerIA/Ines19972Player.php
@@ -14,9 +14,23 @@ class Ines19972Player extends Player
     protected $mySide;
     protected $opponentSide;
     protected $result;
+    protected $max;
 
     public function getChoice()
     {
+        if ($this->result->getNbRound() == 0)
+            return parent::rockChoice();
+        $max = array_search(max($this->result->getStatsFor($this->opponentSide)[scissors], $this->result->getStatsFor($this->opponentSide)[paper],
+                $this->result->getStatsFor($this->opponentSide)[rock]), $this->result->getStatsFor($this->opponentSide));
+        #var_dump($max);
+        if ($max == "paper")
+            return parent::scissorsChoice();
+        if ($max == "rock")
+            return parent::paperChoice();
+        else
+            return parent::rockChoice();
+
+
         // -------------------------------------    -----------------------------------------------------
         // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
         // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
@@ -41,6 +55,6 @@ class Ines19972Player extends Player
         // How can i display the result of each round ? $this->prettyDisplay()
         // -------------------------------------    -----------------------------------------------------
         
-        return parent::paperChoice();            
+        //return parent::RockChoice();
   }
 };
